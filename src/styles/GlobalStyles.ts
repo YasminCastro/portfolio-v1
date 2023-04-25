b74df393d123a4e3021379adf271d5a506626c8e
Yasmin <yasminsdcastro@gmail.com>
2023-04-25 14:33:19 -0300

    feat: scroll bar

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -114,4 +114,19 @@ export default createGlobalStyle`
     padding: 0;
     cursor: pointer;
   }
+
+  *::-webkit-scrollbar,
+  *::-webkit-scrollbar-thumb {
+    width: 26px;
+    border-radius: 13px;
+    background-clip: padding-box;
+    border: 10px solid transparent;
+  }
+
+  *::-webkit-scrollbar-thumb {        
+    box-shadow: inset 0 0 0 10px;
+  }
+
+
+ 
 `;
